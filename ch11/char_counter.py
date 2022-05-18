ad398b6993ae63276d7c9e419e53831477e97ecc
Mohammad Nauman <md.nauman@gmail.com>
2022-05-18 18:02:21 +0000

    Recursion - Char Counter

---
+++ ch11/char_counter.py
@@ -0,0 +1,18 @@
+def char_counter(arr):
+    # Base Case
+    if len(arr) == 0:
+        return 0
+        
+    # Subprogram
+    char_count = len(arr[0]) + char_counter(arr[1:])
+    return char_count
+
+# Unit Tests
+import unittest
+
+class test_char_count(unittest.TestCase):
+    def test_count_x(self):
+        self.assertEqual(char_counter(['ab','c','def','ghij']), 10)
+
+if __name__ == '__main__':
+    unittest.main()
