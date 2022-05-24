b0f3ee5ecb07691d81bf110f45619255ef1dcf93
Mohammad Nauman <md.nauman@gmail.com>
2022-05-24 03:56:03 +0000

    Add a read method to the LL.

--- ch13/LinkedList.py
+++ ch13/LinkedList.py
@@ -1,7 +1,22 @@
 class LinkedList:
+
+    # only thing LL does is hold a reference to the head node
     def __init__(self):
         self.head = None
 
+    # return the data of a node for a given index
+    def read(self, index):
+        current_node = self.head
+        current_index = 0
+
+        while current_index < index:
+            current_node = current_node.next
+            if current_node is None:
+                return 'None'
+            current_index += 1
+        return current_node.data
+        
+    # a simple representation of the LL
     def __repr__(self):
         node = self.head
         nodes = []
@@ -11,4 +26,24 @@ class LinkedList:
             node = node.next
         nodes.append("None")
 
+        # join the nodes using -> separator
         return " -> ".join(nodes)
+
+'''
+from Node import Node
+from LinkedList import LinkedList
+first_node = Node('a')
+second_node = Node('b')
+third_node = Node('c')
+fourth_node = Node('d')
+fifth_node = Node('e')
+first_node.next = second_node
+second_node.next = third_node
+third_node.next = fourth_node
+fourth_node.next = fifth_node
+llist = LinkedList()
+llist.head = first_node
+llist
+llist.read(2)
+
+'''
--- ch13/Node.py
+++ ch13/Node.py
@@ -1,7 +1,10 @@
 class Node:
+
+    # create the node with 1) data and 2) placeholder to reference the next node
     def __init__(self, data):
         self.data = data
         self.next = None
-
+    
+    # a simple node representation 
     def __repr__(self):
         return self.data
