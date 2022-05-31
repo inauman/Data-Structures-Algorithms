201ac6cd7c6d0b9af1aafa5cc392115bc21d4db1
Mohammad Nauman <md.nauman@gmail.com>
2022-05-31 11:27:56 -0700

    Heap - Max for Priority Queues

---
+++ ch16/heap.py
@@ -0,0 +1,11 @@
+class heap:
+    
+    def __init__(self):
+        self.data = []
+        
+    def root_node(self):
+        return self.data[0]
+    
+    def last_node(self):
+        return self.data[-1]
+    
