4affcb6382678dd1af861b6caaca10c44dd45232
Mohammad Nauman <md.nauman@gmail.com>
2022-06-01 09:30:50 -0700

    To store heap in an array and treat it just like a node data structure, added the functions to find out the left child, right child, and parent indexes.

--- ch16/heap.py
+++ ch16/heap.py
@@ -9,3 +9,12 @@ class heap:
     def last_node(self):
         return self.data[-1]
     
+    def left_child_index(self, index):
+        return (index * 2 + 1) # formula: leftChild index = 2 * parent index + 1
+    
+    def rigth_child_index(self, index):
+        return (index * 2 + 2) # formula: rightChild index = 2 * parent index + 2
+    
+    def parent_index(index):
+        # formula parent index = (child index - 1) // 2 (note: integer division ==> math.floor)
+        return (index -1)//2 # note: two slashes "//" are used for integer divison (instead of using math.floor)
