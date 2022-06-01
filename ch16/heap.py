edff88c9485da7c6caf9b3c71df788249c4de6f7
Mohammad Nauman <md.nauman@gmail.com>
2022-06-01 13:29:06 -0700

    delete the root from the heap by swapping with the last node and tricke down the value as appropriate

--- ch16/heap.py
+++ ch16/heap.py
@@ -37,3 +37,47 @@ class heap:
 
             # update the index of the new node
             new_node_index = self.parent_index(new_node_index)
+
+    def delete(self):
+        # only delete the root node from the heap
+        # replace is with "the last node"
+        # and follow the "trickle down" algorithm
+
+        # delete the root by replacing it with the last node
+        self.data[0] = self.data[-1]
+
+        # track the current index of the trickle node
+        trickle_node_index = 0
+
+        # trickle down algorith
+        while self.has_greater_child(trickle_node_index):
+            larger_child_index = self.calculate_larger_child_index(
+                trickle_node_index)
+
+            self.data[trickle_node_index], self.data[larger_child_index] = self.data[larger_child_index], self.data[trickle_node_index]
+
+            trickle_node_index = larger_child_index
+
+    def has_greater_child(self, index):
+
+        value_parent = self.data[index]
+        value_left_child = self.data[self.left_child_index(index)]
+        value_right_child = self.data[self.right_child_index(index)]
+
+        return (value_left_child and value_left_child > value_parent) or (value_right_child and value_right_child > value_parent)
+
+    def calculate_larger_child_index(self, index):
+
+        value_parent = self.data[index]
+        value_left_child = self.data[self.left_child_index(index)]
+        value_right_child = self.data[self.right_child_index(index)]
+
+        # if there is no right child
+        if not value_right_child:
+            return self.left_child_index(index)
+
+        # if right child > left child
+        if value_right_child > value_left_child:
+            return self.rigth_child_index(index)
+        else:
+            return value_left_child(index)
