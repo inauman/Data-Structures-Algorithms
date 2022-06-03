9cf15003e17da6fe3f5c0484a6b2084271bc8ccf
Mohammad Nauman <md.nauman@gmail.com>
2022-06-03 06:01:02 -0700

    updated Vertex to support undirected graphs

--- ch18/Vertex.py
+++ ch18/Vertex.py
@@ -1,9 +1,18 @@
 # Vertex == Node
 class Vertex:
 
-    def __int__(self, value):
+    def __init__(self, value):
         self.value = value
         self.adjacent_vertices = []
 
+    '''def add_adjacent_vertex(self, vertex):
+        self.adjacent_vertices.append(vertex)'''
+    
+    # undirected graph i.e. if adding "alice -> bob", automatically add "bob -> alice"
     def add_adjacent_vertex(self, vertex):
+        if vertex in self.adjacent_vertices:
+            return
         self.adjacent_vertices.append(vertex)
+        vertex.add_adjacent_vertex(self)
+        
+        
